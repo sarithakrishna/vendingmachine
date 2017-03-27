f9808e5f236e56b44af838e42f485d83331c82e5
krish <krish@192.168.0.104>
2017-03-27 08:51:03 -0400

    Initial Version - Implementation of Vending Machine Interface

---
+++ src/main/java/com/audition/VendingMachineImpl.java
@@ -0,0 +1,27 @@
+package com.audition;
+
+import java.util.List;
+
+public class VendingMachineImpl implements VendingMachine {
+
+	public String insertCoin(Coin coin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String selectItem(Item item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Coin> returnChange() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Coin> returnCoins() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
