3ba4e4e1776151f24a92983d314e1dc39d49c7c1
krish <krish@192.168.0.104>
2017-03-27 09:38:34 -0400

    Added fields and method

--- src/main/java/com/audition/Coin.java
+++ src/main/java/com/audition/Coin.java
@@ -1,5 +1,18 @@
 package com.audition;
 
 public class Coin {
+	
+	private double size, weight;
 
+	public Coin(double size, double weight) {
+		this.size = size;
+		this.weight = weight;
+	}
+	
+	public double getValue() {
+		
+		return weight / size ;
+	}
+
+	
 }
