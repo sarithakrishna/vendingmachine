daaaef87bd43721ae9e1c8f98e201efdb2f0f3d4
krish <krish@192.168.0.104>
2017-03-27 08:45:28 -0400

    Added methods in the interface

--- src/main/java/com/audition/VendingMachine.java
+++ src/main/java/com/audition/VendingMachine.java
@@ -1,5 +1,12 @@
 package com.audition;
 
+import java.util.List;
+
 public interface VendingMachine {
+	
+	public String insertCoin(Coin coin);
+	public String selectItem(Item item);
+	public List<Coin> returnChange();
+	public List<Coin> returnCoins();
 
 }
