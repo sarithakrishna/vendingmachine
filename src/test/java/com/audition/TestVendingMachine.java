4f8f9042a6e7d5b47116e5a40131bc8a0563beb9
krish <krish@192.168.0.104>
2017-03-27 09:47:28 -0400

    Test insertCoin initial version

--- src/test/java/com/audition/TestVendingMachine.java
+++ src/test/java/com/audition/TestVendingMachine.java
@@ -5,15 +5,20 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestVendingMachine {
-	
+
 	VendingMachineImpl machine = new VendingMachineImpl();
-	
+
 	@Test
 	public void testDefaultDisplayAssertEquals() {
-		Coin coin = new Coin(0.0,0.0);
-		assertEquals(machine.insertCoin(coin),"Insert Coin");
-		
+		Coin coin = new Coin(0.0, 0.0);
+		assertEquals(machine.insertCoin(coin), "Insert Coin");
+
 	}
-	
+
+	@Test
+	public void testInsertCoinAssertEquals() {
+		Coin coin = new Coin(1.0, 0.5);
+		assertEquals(machine.insertCoin(coin), null);
+	}
 
 }
