6d6a0448d382d753f8a3c3bed2c92a22041af80e
krish <krish@192.168.0.104>
2017-03-27 09:45:17 -0400

    Test DefaultDisplay in the VendingMachine

--- src/test/java/com/audition/TestVendingMachine.java
+++ src/test/java/com/audition/TestVendingMachine.java
@@ -1,5 +1,19 @@
 package com.audition;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestVendingMachine {
+	
+	VendingMachineImpl machine = new VendingMachineImpl();
+	
+	@Test
+	public void testDefaultDisplayAssertEquals() {
+		Coin coin = new Coin(0.0,0.0);
+		assertEquals(machine.insertCoin(coin),"Insert Coin");
+		
+	}
+	
 
 }
