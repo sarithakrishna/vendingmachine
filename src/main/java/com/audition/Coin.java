89150682f237a60f6979bd4a4828d081d5432742
krish <krish@192.168.0.104>
2017-03-30 12:04:57 -0400

    Added Hashcode and equals method in the Coin class

--- src/main/java/com/audition/Coin.java
+++ src/main/java/com/audition/Coin.java
@@ -1,18 +1,45 @@
 package com.audition;
 
 public class Coin {
-	
+
 	private double size, weight;
 
 	public Coin(double size, double weight) {
 		this.size = size;
 		this.weight = weight;
 	}
-	
+
 	public double getValue() {
-		
-		return weight / size ;
+
+		return weight / size;
 	}
 
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(size);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(weight);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Coin other = (Coin) obj;
+		if (Double.doubleToLongBits(size) != Double.doubleToLongBits(other.size))
+			return false;
+		if (Double.doubleToLongBits(weight) != Double.doubleToLongBits(other.weight))
+			return false;
+		return true;
+	}
+
 }
