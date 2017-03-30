6306a0f678d8fb97b1efdf0cce94410249dca5b0
krish <krish@192.168.0.104>
2017-03-30 14:07:00 -0400

    Added for user defined exception

--- src/main/java/com/audition/InvalidInsertCoinsException.java
+++ src/main/java/com/audition/InvalidInsertCoinsException.java
@@ -2,6 +2,7 @@ package com.audition;
 
 public class InvalidInsertCoinsException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
 	private String message;
 
 	public InvalidInsertCoinsException(String message) {
