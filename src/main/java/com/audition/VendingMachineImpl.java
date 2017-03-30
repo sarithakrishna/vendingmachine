36955f50ab27d9e30934c35ed563731e6e8c278f
krish <krish@192.168.0.104>
2017-03-30 12:04:19 -0400

    Implemented insert coin method when valid coins entered in the machine

--- src/main/java/com/audition/VendingMachineImpl.java
+++ src/main/java/com/audition/VendingMachineImpl.java
@@ -1,15 +1,33 @@
 package com.audition;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class VendingMachineImpl implements VendingMachine {
 
 	private String defaultDisplayText = "Insert Coin";
+	private double currentBalance = new Double(0.0);
+
+	private static List<Coin> validCoin = new ArrayList<Coin>();
+
+	static {
+		validCoin.add(new Coin(1, 0.50));
+		validCoin.add(new Coin(1, 0.25));
+		validCoin.add(new Coin(1, 0.10));
+	}
 
 	public String insertCoin(Coin coin) {
 		// TODO Auto-generated method stub
 		if (coin.getValue() > 0) {
-			return null;
+			if (validCoin.contains(coin)) {
+
+				currentBalance = currentBalance + coin.getValue();
+				return Double.toString(currentBalance);
+
+			} else {
+				return null;
+			}
+
 		} else {
 			return defaultDisplayText;
 		}
