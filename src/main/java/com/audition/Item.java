fa5dad2fc6b9ba0502e35bde9b07d9c7c94f3f02
krish <krish@192.168.0.104>
2017-03-30 12:45:50 -0400

    Added member variables for Item class

--- src/main/java/com/audition/Item.java
+++ src/main/java/com/audition/Item.java
@@ -1,5 +1,50 @@
 package com.audition;
 
 public class Item {
+	
+	public Item(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	private String name;
+	private double price;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
+		return true;
+	}
 
 }
